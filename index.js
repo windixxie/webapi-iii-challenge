6ed94b91a3d8ab917aec4eddff336c88058fbe52
Winnie Song <winniesong0121@gmail.com>
2019-07-10 22:09:40 -0400

    logger added

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code away!
+const express = require("express");
+const port = 4000;
+
+const logger = require("./middleware/logger.js");
+const userRouter = require("./users/userRouter.js");
+const postRouter = require("./posts/postRouter.js");
+
+const server = express();
+
+server.use(logger);
+
+server.get("/", (req, res) => {
+  res.status(200).send(`
+    <h2>Node Blog API</h2>
+    <p>Welcome to the Node Blog API</p>
+  `);
+});
+
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
